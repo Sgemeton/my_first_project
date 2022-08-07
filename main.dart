628d5af2af8709bffd5ff53fee32b361c53e6d45
Iliya <Glorifiednoobie@gmail.com>
2022-08-07 16:20:02 +0430

    first commit

---
+++ main.dart
@@ -0,0 +1,5 @@
+main(){
+  print("Hello World!");
+  print("My name is Iliya Harati!");
+  print("This is my first Dart Project and I'm excited to learn more about this useful programming language!");
+}
